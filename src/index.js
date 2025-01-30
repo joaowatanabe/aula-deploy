583256f84a77743511fd143af108d3e19ec819d5
wabemusic <jvwatanabe@gmail.com>
2025-01-30 16:30:09 -0300

    teste get

--- src/index.js
+++ src/index.js
@@ -8,32 +8,13 @@ app.use(express.json());
 
 app.get("/", async (req, res) => {
   try {
-    const carros = await knex('carros');
+    const carros = await knex("carros");
 
-
+    return res.json(carros);
   } catch (error) {
     return res.status(400).json({ error: error.message });
   }
 });
-
-app.post("/", async (req, res) => {
-  try {
-    const tableExists = await knex.schema.hasTable('carros');
-
-    if(tableExists) {
-      return res.status(400).json({ message: 'Table already exists' });
-    }
-
-    const createTable = await knex.schema.createTable('carros', (table) => {
-      table.increments('id');
-      table.string('modelo');
-    });
-
-    return res.status(201).json({ createTable });
-  } catch (error) {
-    return res.status(400).json({ error: error.message });
-  }
-})
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Server is running on port ${process.env.PORT || 3000}`);
